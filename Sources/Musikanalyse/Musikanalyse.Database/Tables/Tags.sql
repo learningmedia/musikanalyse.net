a901a182a3c777d9df2d5ed8325873ac32186d14
Ulrich Kaiser <ulrich.kaiser@hmtm.de>
2012-11-10 23:16:59 +0000

    Added first draft for requirements and new database project in solution.
    git-tfs-id: [https://uli.visualstudio.com:443/DefaultCollection]$/musikanalyse.net;C33

---
+++ Sources/Musikanalyse/Musikanalyse.Database/Tables/Tags.sql
@@ -0,0 +1,5 @@
+﻿CREATE TABLE [dbo].[Tags]
+(
+  [Id] INT NOT NULL PRIMARY KEY IDENTITY, 
+  [Value] NVARCHAR(50) NOT NULL
+)
---
+++ Sources/Musikanalyse/Musikanalyse.Database/Tables/Tutorials.sql
@@ -0,0 +1,5 @@
+﻿CREATE TABLE [dbo].[Tutorials]
+(
+  [Id] INT NOT NULL PRIMARY KEY IDENTITY,
+  [ReleaseState] INT NOT NULL
+)
