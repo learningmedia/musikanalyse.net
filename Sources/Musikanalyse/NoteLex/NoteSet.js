d80138b9e6691c78e847c8ba570838b74d1bdbb8
Ulrich Kaiser <ulrich.kaiser@hmtm.de>
2013-07-14 21:18:22 +0000

    Implemented method fromMidiValues in class NoteSet and complemented validation in the constructor.
    git-tfs-id: [https://uli.visualstudio.com:443/DefaultCollection]$/musikanalyse.net;C92

--- Sources/Musikanalyse/NoteLex/NoteSet.js
+++ Sources/Musikanalyse/NoteLex/NoteSet.js
@@ -4,16 +4,37 @@ var NoteLex;
         function NoteSet(base, intervals) {
             this.base = base;
             this.intervals = intervals;
+            var result = base / base;
+            if (result != base) {
+                throw new Error("Base value is not a integer.");
+            }
+            var isValidArr = _.all(intervals, function (x) {
+                var test = x / x;
+                return x == test;
+            });
+            if (!isValidArr) {
+                throw new Error("Some of the intervals value is not a integer.");
+            }
             if (base < 0 || base > 11) {
                 throw new Error("Base value is out of range.");
             }
-
             if (_.some(intervals, function (x) {
                 return x < 0 || x > 11;
             })) {
                 throw new Error("Some of the interval values are out of range.");
             }
         }
+        NoteSet.fromMidiValues = function (midiValues) {
+            midiValues = _.sortBy(midiValues, function (x) {
+                return x;
+            });
+            midiValues = _.map(midiValues, function (x) {
+                return x % 12;
+            });
+            midiValues = _.unique(midiValues);
+            var base = midiValues.length == 0 ? 0 : midiValues[0];
+            return new NoteSet(base, midiValues);
+        };
         return NoteSet;
     })();
     NoteLex.NoteSet = NoteSet;
--- Sources/Musikanalyse/NoteLex/NoteSet.ts
+++ Sources/Musikanalyse/NoteLex/NoteSet.ts
@@ -3,14 +3,33 @@
 
 module NoteLex {
     export class NoteSet implements INoteSet {
+
         constructor(public base: number, public intervals: number[]) {
+            var result = base / base;
+            if (result != base) {
+                throw new Error("Base value is not a integer.");
+            }
+            var isValidArr = _.all(intervals, x => {
+                var test = x / x;
+                return x == test;
+            })
+            if (!isValidArr) {
+                throw new Error("Some of the intervals value is not a integer.");
+            }
             if (base < 0 || base > 11) {
                 throw new Error("Base value is out of range.");
             }
-
             if (_.some(intervals, x => x < 0 || x > 11)) {
                 throw new Error("Some of the interval values are out of range.");
             }
+        }
+
+        static fromMidiValues(midiValues: number[]): INoteSet {
+            midiValues = _.sortBy(midiValues, x => x); 
+            midiValues = _.map(midiValues, x => x % 12);
+            midiValues = _.unique(midiValues);
+            var base = midiValues.length == 0 ? 0 : midiValues[0];
+            return new NoteSet(base, midiValues);
         }
     }
 }
