5a984e8200d4e68bdf1b71bdebc4614c1ebe6867
Andreas Helmberger <public@ahelmberger.de>
2013-07-21 20:25:46 +0000

    QUnit tests for NoteSet.
    git-tfs-id: [https://uli.visualstudio.com:443/DefaultCollection]$/musikanalyse.net;C99

--- Sources/Musikanalyse/NoteLex/UnitTests/NoteSetTests.ts
+++ Sources/Musikanalyse/NoteLex/UnitTests/NoteSetTests.ts
@@ -3,29 +3,34 @@
 
 QUnit.module("NoteSetTests");
 
-test("Constructor should throw if base is less than 0.", function () {
-    throws(() => new NoteLex.NoteSet(-1, [0, 1, 2]));
+test("Constructor should create an empty set if no argument is supplied.", function () {
+    var noteSet = new NoteLex.NoteSet();
+    ok(isNaN(noteSet.base));
+    strictEqual(noteSet.intervals.length, 0);
 });
 
-test("Constructor should throw if any interval value is less than 0.", function () {
-    throws(() => new NoteLex.NoteSet(3, [0, -1, 2]));
+test("Constructor should create an empty set if an empty array is supplied as argument.", function () {
+    var noteSet = new NoteLex.NoteSet([]);
+    ok(isNaN(noteSet.base));
+    strictEqual(noteSet.intervals.length, 0);
 });
 
-test("Constructor should throw if interval values do not contain 0.", function () {
-    throws(() => new NoteLex.NoteSet(3, [1, 2, 3]));
+test("Constructor should keep smallest value as base value.", function () {
+    var noteSet = new NoteLex.NoteSet([9, 5, 7, 17, 5]);
+    strictEqual(noteSet.base, 5);
 });
 
-test("Constructor should not throw if intervals is an empty array.", function () {
-    var noteSet = new NoteLex.NoteSet(0, []);
-    deepEqual(noteSet.intervals, []);
+test("Constructor should transpose base value to a value between 0 and 11 for values greater than 11.", function () {
+    var noteSet = new NoteLex.NoteSet([14, 16, 18]); // d-e-f#
+    strictEqual(noteSet.base, 2);
 });
 
-test("Constructor should order interval values that are not in ascending order.", function () {
-    var noteSet = new NoteLex.NoteSet(3, [3, 0, 1]);
-    deepEqual(noteSet.intervals, [0, 1, 3]);
+test("Constructor should transpose base value to a value between 0 and 11 for values less than 0.", function () {
+    var noteSet = new NoteLex.NoteSet([-3, 0, 4]); // a-c-e
+    strictEqual(noteSet.base, 9);
 });
 
-test("Constructor should discard reduntant interval values.", function () {
-    var noteSet = new NoteLex.NoteSet(3, [0, 1, 3, 4, 3, 5]);
-    deepEqual(noteSet.intervals, [0, 1, 3, 4, 5]);
+test("Constructor should normalize and order all values.", function () {
+    var noteSet = new NoteLex.NoteSet([9, 5, 7, 17, 5]);
+    deepEqual(noteSet.intervals, [0, 2, 4]);
 });
