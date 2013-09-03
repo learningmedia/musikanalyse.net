8edd25645088c3951e08a29cf3eaca399c2c32ee
Ulrich Kaiser <ulrich.kaiser@hmtm.de>
2013-09-03 21:37:02 +0000

    PcSetHelper.GetIntervalVector and Tests implemented.
    git-tfs-id: [https://uli.visualstudio.com:443/DefaultCollection]$/musikanalyse.net;C120

--- Sources/Musikanalyse/PcSetTableGenerator/PcSetHelper.cs
+++ Sources/Musikanalyse/PcSetTableGenerator/PcSetHelper.cs
@@ -23,8 +23,38 @@
 
         public static int[] GetIntervalVector(PcSet primeForm)
         {
-            // TODO: Implement:
-            return new int[0];
+            int count = primeForm.Count;
+            if (count < 2)
+            {
+                return new[] { 0, 0, 0, 0, 0, 0 };
+            }
+
+            List<int> values = new List<int>();
+            for (int x = 0; x < count - 1; x++)
+            {
+                int[] actualArr = primeForm.Skip(x).ToArray();
+                for (int y = 1; y <= actualArr.Length - 1; y++)
+                {
+                    int value = actualArr[y] - actualArr[y - y];
+                    if (value < 6)
+                    {
+                        values.Add(value);
+                    }
+                    else
+                    {
+                        values.Add(12 - value);
+                    }
+                }
+            }
+
+            int smallSecond = values.FindAll(x => x == 1).Count; 
+            int greatSecund = values.FindAll(x => x == 2).Count; 
+            int smallThird = values.FindAll(x => x == 3).Count; 
+            int greatThird = values.FindAll(x => x == 4).Count; 
+            int quart = values.FindAll(x => x == 5).Count; 
+            int tritone = values.FindAll(x => x == 6).Count; 
+
+            return new[] { smallSecond, greatSecund, smallThird, greatThird, quart, tritone };
         }
     }
 }
