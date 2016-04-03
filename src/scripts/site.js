81000b25a0cb21fd9b0cd758cc962c9b3fba0932
Ulrich Kaiser <ulrich.kaiser@hmtm.de>
2016-04-03 23:46:40 +0200

    Add tutorial page and sub pages

--- src/scripts/site.js
+++ src/scripts/site.js
@@ -2,26 +2,7 @@ $(function () {
   var buttons = $('[data-role=category-button]');
   var sections = $('[data-role=category-section]');
 
-  function getCurrentCategoryFromUrl() {
-    return (window.document.location.hash || '#').replace(/^#/, '') || 'all';
-  }
-
-  function showCategory(category) {
-    sections.toggleClass('is-active', false);
-    buttons.toggleClass('is-active', false);
-    sections.filter('[data-category=' + category + ']').toggleClass('is-active', true);
-    buttons.filter('[data-category=' + category + ']').toggleClass('is-active', true);
-  }
-
-  var initialCategory = getCurrentCategoryFromUrl();
-  showCategory(initialCategory);
-
-  window.addEventListener('hashchange', function (evt) {
-    var activeCategory = getCurrentCategoryFromUrl();
-    showCategory(activeCategory);
-  });
-
-  $('.exercise').each(function () {
+    $('.exercise').each(function () {
     var exercise = $(this);
     $('<a href="#">Übung anzeigen</a>').on('click', function () {
       openExercise(exercise.get(0).innerHTML);
