1ffb28cf85826306fd808743b00be8507ad71aab
Ulrich Kaiser <Ulrich Kaiser>
2017-05-19 00:43:51 +0200

    Refactor gb exercise

--- src/scripts/gbExercises.js
+++ src/scripts/gbExercises.js
@@ -1,74 +1,130 @@
-var gbExercises = {
-	"exercise-1a": {
-		"image": "GbAufgabe_0001.png",
-	  "startKey": 62,
-	  "rightValues": [62, 65, 69],
-	  "leadingTone": 0
-	},
-	"exercise-1b": {
-		"image": "GbAufgabe_0002.png",
-	  "startKey": 65,
-	  "rightValues": [65, 69, 72],
-	  "leadingTone": 0
-	},
-	"exercise-2a": {
-		"image": "",
-	  "startKey": 63,
-	  "rightValues": [63, 67, 72],
-	  "leadingTone": 0
-	}, 
-	"exercise-2b": {
-		"image": "",
-	  "startKey": 67,
-	  "rightValues": [67, 71, 76],
-	  "leadingTone": 0
-	}, 
-	"exercise-3a": {
-		"image": "",
-	  "startKey": 64,
-	  "rightValues": [64, 67, 73],
-	  "leadingTone": 73
-	}, 
-	"exercise-3b": {
-		"image": "",
-	  "startKey": 65,
-	  "rightValues": [65, 69, 75],
-	  "leadingTone": 0
-	}
+var images = ["60_c.png", "61_cis.png", "61_des.png", "62_d.png", "63_dis.png", "63_es.png", "64_64.png", "65_f.png", "66_fis.png", "66_ges.png", "67_g.png", "68_as.png", "68_gis.png", "69_a.png", "70_ais.png", "70_b.png", "71_h.png"]
+var gbChiffres = ["4_0-5-7.png", "6_0-3-8.png"]
+var exercises = [];
+
+class Exercise {
+
+  constructor(startKey, keyName, chiffre, chiffreValues, noteImagePath, chiffreImagePath) {
+    this.startKey = startKey;
+    this.keyName = keyName;
+    this.chiffre = chiffre;
+    this.chiffreValues = chiffreValues;
+    this.originalValues = this._getOriginalValues();
+    this.noteImagePath = noteImagePath;
+    this.chiffreImagePath = chiffreImagePath
+    this.rightValues = [];
+    this.wrongValues = [];
+  }
+
+  _getOriginalValues() {
+    var keys = [];
+    keys.push(this.startKey);
+    for (var i = 0; i < this.chiffreValues.length; i++) {
+    if(this.chiffreValues[i] == 0) continue;
+    keys.push(this.startKey + this.chiffreValues[i])
+    }
+    return keys;
+  }
 }
 
-var lastIds = [];
+function createExercise(id) {
+	const folderName = "/content/tutorials/generalbass/";
+	var randomImageNumber = Math.floor((Math.random() * images.length));
+	var noteImageName = images[randomImageNumber];
 
-function showValue(exerciseNumber) {
-	//Hier wird die Übung aus dem JSON-Objekt geholt
-	var id = 'exercise-' + exerciseNumber;
-	var exercise = gbExercises[id];
-	var selectedKeys = $('#' + id).klavier('getSelectedValues');
+	var imageNameParts = noteImageName.split("_");
+	var startKey = parseInt(imageNameParts[0], 10);
+	var keyName = imageNameParts[1].replace('.png', '');
+
+	var randomGbNumber = Math.floor((Math.random() * gbChiffres.length));
+	var chiffreImageName = gbChiffres[randomGbNumber];
+	var chiffreParts = chiffreImageName.split("_");
+	var chiffre = chiffreParts[0];
+	var chiffreValues = chiffreParts[1].replace('.png', '');
+
+	var exercise = new Exercise(startKey, keyName, 
+		createArray(chiffre), 
+		createArray(chiffreValues), 
+		folderName + noteImageName, 
+		folderName + chiffreImageName);
+
+	createPiano(id, exercise.startKey)
+	exercises.push(exercise);
+	return exercise;
+}
+
+function createPiano(id, startKey) {
+	$('#exercise').klavier({ startKey: 60, endKey: 95, onSelectedValuesChanged: onValueChanged });
+  $('#exercise').klavier('setSelectedValues', startKey + '');
+}
+
+function createArray(value){
+	var parts = value.split('-');
+	var arr = [];
+	for (var i = 0; i < parts.length; i++) {
+		var part = parts[i];
+		arr.push(parseInt(part, 10));
+	}
+	return arr;
+}
+
+var onValueChanged = function() {
+
+}
+
+function showValue() {
+	var exercise = exercises[exercises.length -1];
+	var selectedKeys = $('#exercise').klavier('getSelectedValues');
 
 	//Hier wird der vorgegebene Ton aus den Selektion entfernt
-	var index = selectedKeys.indexOf(exercise.startKey);
+	var startKey = exercise.startKey;
+	var originValues = exercise.originalValues;
+	var index = selectedKeys.indexOf(startKey);
 	selectedKeys.splice(index, 1);
 
-	//Hier werden die richtigen und falschen Tasten ermittelt und die Farben gesetzt
+	//Hier werden die falschen Tasten ermittelt und die Farben gesetzt
 	var rightValues = [];
 	var wrongValues = [];
 	for (var i = 0; i < selectedKeys.length; i++) {
-		if(evaluateValue(selectedKeys[i], exercise.rightValues, exercise.startKey)){
+		if(evaluateValue(selectedKeys[i], originValues, startKey)){
 			rightValues.push(selectedKeys[i]);			
 		} else {
 			wrongValues.push(selectedKeys[i]);
 		}
-	} 
-	setColors(id, rightValues, wrongValues);
-	checkPlayability(id, rightValues, selectedKeys);
+	}
+	exercise.rightValues = rightValues;
+	exercise.wrongValues = wrongValues;
+
+	//Hier werden die Farben gesetzt
+	setColors(exercise);
 }
 
-function evaluateValue(value, rightValues, startkey) {
+function evaluateValue(value, originValues, startkey) {
 	//Hilfsfunktion zum Ermitteln der richtigen Tasten in den verschiedenen Oktavlagen
 	while ((value - 12) >= startkey) {				
 		value = value - 12;
 	}
-	return rightValues.indexOf(value) >= 0;
+	return originValues.indexOf(value) >= 0;
+}
+
+function setColors(exercise) {
+	exercise.wrongValues.forEach(function(element, index, array) {
+		var searchId = "exercise > .klavier-key[data-value=" + element + "]";
+		var wrongKey = $('#' + searchId);
+		wrongKey.removeClass('klavier-selected-key');
+		wrongKey.addClass('wrongColor');
+	});
+	exercise.rightValues.forEach(function(element, index, array) {
+		var searchId = "exercise > .klavier-key[data-value=" + element + "]";
+		var rightKey = $('#' + searchId);
+		rightKey.removeClass('klavier-selected-key');
+		rightKey.addClass('rightColor');
+	});
+}
+
+function resetColors() {
+	$('.wrongColor').removeClass('wrongColor');
+	$('.rightColor').removeClass('rightColor');
 }
 
 function checkPlayability(id, rightValues, selectedValues) {
@@ -99,15 +155,3 @@ function checkPlayability(id, rightValues, selectedValues) {
 		$('#' + leadingToneId + "-doubleLeadingTone").hide();
 	}
 }
-
-function setColors(id, rightValues, wrongValues) {
-	wrongValues.forEach(function(element, index, array) {
-		var searchId = id + " > .klavier-key[data-value=" + element + "]";
-		$('#' + searchId).css("background-color", "#FF3C3C");
-	});
-	rightValues.forEach(function(element, index, array) {
-		var searchId = id + " > .klavier-key[data-value=" + element + "]";
-		$('#' + searchId).css("background-color", "lightgreen");
-	});
-}
-
