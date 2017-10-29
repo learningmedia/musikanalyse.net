57a85cc343802fa0271d3dcecbf25c81ce2fea9c
musikisum <ulrich.kaiser@hmtm.de>
2017-10-29 23:10:07 +0100

    Repair foldable.js

--- src/scripts/foldable.js
+++ src/scripts/foldable.js
@@ -2,12 +2,30 @@ var element;
 var elements = document.getElementsByClassName("foldable");
 
 for (var i = 0; i < elements.length; i++) {
-	var btn = document.createElement("BUTTON");
-	var link = 'foldable-' + i;
-	btn.classList.add(link);
-	var text = document.createTextNode("Erläuterungen verbergen");
-	btn.appendChild(text);
-	element = elements[i];
-	element.id = link;
-	element.before(btn);
+  var btn = document.createElement("BUTTON");
+  var link = 'toggleId-' + i;
+  btn.classList.add(link);
+  var text = document.createTextNode("Erläuterungen verbergen");
+  btn.appendChild(text);
+  btn.addEventListener('click', foldableClick);
+
+  element = elements[i];
+  element.id = link;
+  element.parentNode.insertBefore(btn, element);
 }
+
+function foldableClick(event) {
+  var elem = event.target;
+  var cssClass = elem.classList[0];
+  debugger;
+  var container = document.getElementById(cssClass);
+  if (container) {
+    if (container.style.display === 'none') {
+      container.style.display = 'block';
+      elem.value = "Erläuterungen verbergen";
+    } else {
+      container.style.display = 'none';
+      elem.value = "Erläuterungen anzeigen";
+    }
+  }
+}  
