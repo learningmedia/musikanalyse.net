163cd3c16df6c00c10bcf65d851a5a9516b388b0
Ulrich Kaiser <ulrich.kaiser@hmtm.de>
2017-11-07 14:01:31 +0100

    Expand foldable.js and new implementation in scale tutorial

--- src/scripts/foldable.js
+++ src/scripts/foldable.js
@@ -2,34 +2,36 @@ var element;
 var elements = document.getElementsByClassName("foldable");
 
 for (var i = 0; i < elements.length; i++) {
-  var btn = document.createElement("BUTTON");
-  var link = 'toggleId-' + i;
-  btn.classList.add(link);
-  var text = document.createTextNode("Weitere Erläuterungen anzeigen");
-  btn.appendChild(text);
-  btn.addEventListener('click', foldableClick);
-  btn.classList.add('foldable-collapsed');
-
   element = elements[i];
+  var text = element.getAttribute("data-text");
+  var anchor = document.createElement("a");
+  var link = 'toggleId-' + i;
+  anchor.classList.add(link);
+  var text = document.createTextNode(text + " - anzeigen");
+  anchor.appendChild(text);
+  anchor.addEventListener('click', foldableClick);
+  anchor.classList.add('foldable-collapsed');
+  anchor.classList.add('clickable');
+
   element.id = link;
-  element.parentNode.insertBefore(btn, element);
+  element.parentNode.insertBefore(anchor, element);
   element.style.display = 'none';
 }
 
 function foldableClick(event) {
   var elem = event.target;
+  var text = elem.innerHTML;
   var cssClass = elem.classList[0];
-  console.log(cssClass);
   var container = document.getElementById(cssClass);
   if (container) {
     if (container.style.display === 'none') {
       container.style.display = 'block';
-      elem.firstChild.data = "Erläuterungen verbergen";
+      elem.firstChild.data = text.replace('anzeigen', 'verbergen');
       elem.classList.remove('foldable-collapsed');
       elem.classList.add('foldable-block');
     } else {
       container.style.display = 'none';
-      elem.firstChild.data = "Weitere Erläuterungen anzeigen";
+      elem.firstChild.data = text.replace('verbergen', 'anzeigen');
       elem.classList.remove('foldable-block');
       elem.classList.add('foldable-collapsed');
     }
