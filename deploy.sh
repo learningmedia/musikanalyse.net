6a0d7e3c6c59369f86050625f7be8961f4e8d798
Andreas Helmberger <public@ahelmberger.de>
2018-03-04 22:18:00 +0100

    Fix deployment on new CI infrastructure

--- deploy.sh
+++ deploy.sh
@@ -5,7 +5,9 @@ envsubst < ./build/web.config.template > ./build/web.config
 rm ./build/web.config.template
 
 mkdir -p ~/.lftp
-echo "set ssl:verify-certificate no" > ~/.lftp/rc
+echo "set dns:order inet" >> ~/.lftp/rc
+echo "set ssl:check-hostname no" >> ~/.lftp/rc
+echo "set ssl:verify-certificate no" >> ~/.lftp/rc
 
 FTP_URL="ftp://$FTP_USER:$FTP_PASS@$FTP_HOST"
 LCD="./build"
